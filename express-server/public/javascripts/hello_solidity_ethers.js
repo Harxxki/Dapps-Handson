23192fd6781093f9439657fc2cf62ac7ca817002
Haruki <48507923+hwavy@users.noreply.github.com>
2021-07-04 14:58:01 +0900

    WIP

--- express-server/public/javascripts/hello_solidity_ethers.js
+++ express-server/public/javascripts/hello_solidity_ethers.js
@@ -1,32 +1,31 @@
-const { ethers } = require("ethers");
+const ethers = require('ethers');
 
-let myContract;
-
-const ContractAddress = "0xB7AC107628ed7D87fFd04d4B8F76d0BC2C07A727"; // コントラクトアドレス
-const ContractAbi = [
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "get",
-        "outputs": [
-          {
-            "internalType": "string",
-            "name": "",
-            "type": "string"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "pure",
-        "type": "function"
-    }
-  ];
+const address = "0xB7AC107628ed7D87fFd04d4B8F76d0BC2C07A727"; // コントラクトアドレス
+const abi = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "pure",
+      "type": "function"
+  }
+];
+const provider = new ethers.providers.Web3Provider(window.ethereum);
   
 window.onload = function() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner(0);
-  myContract = new ethers.Contract(ContractAddress, ContractAbi, signer);
+  var contract = new ethers.Contract(address,abi,provider);
 
-  myContract.get((error, result) => {
+  var callPromise = contract.getValue();
+
+  callPromise.then(function(result){
     document.getElementById("contract_result").textContent = result;
   });
 };
